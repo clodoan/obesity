fa615a849f0455c93f43741235c4e02f77c7ac03
Claudio Angrigiani <angrigianicr@gmail.com>
2020-05-06 12:42:11 -0400

    obesity revisited

--- circles.py
+++
@@ -1,4 +0,0 @@
-# 1 through 8
-for i in range(1, 9):
-    with open ("circle-"+ str(i) + ".svg", "w") as f:
-         f.write("<svg height='8' width='8'> <circle cx='4' cy='4' r='" + str(i/2) + "' fill='#224444' /> </svg>")
